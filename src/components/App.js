9643991bc23b6dea99c09d48a4ca6eebcc711c36
Barinulka <barinulka@yandex.ru>
2021-07-24 17:03:22 +0400

    2. Lesson 1. Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import './App.scss';
 import Message from './Message/Message';
 
+const message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum veniam ipsa dolores cumque error at adipisci, ipsam dolorum voluptatem magni!';
+
 function App() {
-  let message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum veniam ipsa dolores cumque error at adipisci, ipsam dolorum voluptatem magni!';
   return (
     <div className="App center">
       <Message text={message} title="Lorem title" />
