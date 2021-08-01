1e2091bddfaf0dfbce536f5f96a46f98b8bdd613
Barinulka <barinulka@yandex.ru>
2021-08-01 15:20:44 +0400

    7. Lesson 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useCallback, useState } from 'react';
+import { TextField, Fab, Button } from '@material-ui/core';
+import { ThemeProvider, useTheme, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FF9800",
+    },
+    secondary: {
+      main: "#0098FF",
+    },
+  },
+ });
+ 
 
 function App() {
+
+  const [value, setValue] = useState('');
+
+  const handleChange = useCallback((e) => {
+    setValue(e.target.value)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ThemeProvider theme={theme}>
+
+      <TextField 
+        style={{margin: '20px'}}
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        value={value}
+        onChange={handleChange}
+      />
+    </ThemeProvider>
+      <Fab color="secondary" aria-label="edit">
+        Edit!
+      </Fab>
+
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
   );
 }
 
