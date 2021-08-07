82ff2e91296f1405270e5823ad3e440c4aa299ee
Barinulka <barinulka@yandex.ru>
2021-08-07 11:50:55 +0400

    11. Lesson 3. Fix

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,30 @@ import { ChatList } from './ChatList/ChatList';
 
 export const App = () => {
 
+
   const [messages, setMessages] = useState([]);
 
   const handleSendMessages = useCallback((newMessage) => {
     setMessages([...messages, newMessage]);
   }, [messages]);
 
-  useEffect(() => {
-    const roboMesArr = ['Hello', 'JavaScript', 'ReactJS', 'Hello there', 'quis lectus nulla at volutpat diam ut venenatis tellus in', 'leo in vitae turpis massa sed elementum tempus egestas sed', 'dignissim enim sit amet venenatis urna'];
-    if (messages.length && messages[messages.length - 1].name !== 'Robot') {
-        const robotMess = { name: 'Robot', text: arrayRandElement(roboMesArr), id: Date.now()};
-        setTimeout( () => setMessages([...messages, robotMess]), 1500 );
-    }
-  }, [messages])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const roboMesArr = ['Hello', 'JavaScript', 'ReactJS', 'Hello there', 'quis lectus nulla at volutpat diam ut venenatis tellus in', 'leo in vitae turpis massa sed elementum tempus egestas sed', 'dignissim enim sit amet venenatis urna'];
 
   const arrayRandElement = (arr) => {
     let rand = Math.floor(Math.random() * arr.length);
     return arr[rand];
   }
+
+  useEffect(() => {
+
+    if (messages.length && messages[messages.length - 1].name !== 'Robot') {
+        const robotMess = { name: 'Robot', text: arrayRandElement(roboMesArr), id: Date.now()};
+        setTimeout( () => setMessages([...messages, robotMess]), 1000 );
+    }
+  }, [messages, roboMesArr]);
+
+ 
 
   return (
     <Container fixed>
@@ -34,7 +40,9 @@ export const App = () => {
         <div className="left-field">
           <ChatList />
         </div>
+
         <div className="right-field">
+
           <div className="message-field">
             <MessageField messages={messages} />
           </div>
@@ -42,6 +50,7 @@ export const App = () => {
           <div className="form-field">
             <MessageForm onSendMessages={handleSendMessages}/>
           </div>
+          
         </div>
         
       </div>
