d27b222326d3c6dd735cee842a882306345810be
Barinulka <barinulka@yandex.ru>
2021-08-18 14:17:42 +0400

    Fixed

--- src/components/MessageField/MessageField.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Message } from '../Message/Message';
-
-export const MessageField = ({ messageList }) => {
-    return messageList.map((mess) => (
-        <Message author={mess.author} message={mess.message} key={mess.id} />
-    ));
-}
