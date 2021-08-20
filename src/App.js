0221cc80cd042f48f40aa8c5cfd741d9a675b07a
Barinulka <barinulka@yandex.ru>
2021-08-20 17:56:21 +0400

    14. Lesson 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Router } from "./components/Router";
 import './App.scss';
+import { MyNewContext } from "./context/newContext";
 
 function App() {
     return (
-        <Router />
+        <MyNewContext.Provider value={{mess: 'Hi there!'}}>
+            <Router />
+        </MyNewContext.Provider>
     );
 }
 
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MyNewContext } from '../../context/newContext';
 
 export const Profile = () => {
+
+    const contextValue = useContext(MyNewContext);
+
+    
+
+    function withLogger(fn) {
+        return function(...args) {
+          console.log(args);
+          return fn(...args);
+        }
+      }
+      
+      const foo = (a, b) => a + b;
+      const bar = withLogger(foo);
+      
+      console.log(bar(1, 2));
+
     return (
-        <h2>Страница профиля</h2>
+        <>
+            <h2>Страница профиля</h2>
+            <span>{contextValue.mess}</span>
+        </>
     );
 }
